c41dd550dcaf8fe24b6b2861fda83c3fc0050098
Vagner Cardoso <vagnercardosoweb@gmail.com>
2022-06-27 21:18:25 -0300

    chore: move error creation to a function
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/utils/http-request.ts
+++ src/utils/http-request.ts
@@ -24,25 +24,23 @@ const jsonParse = (value: any) => {
 	}
 };
 
+const makeError = (e: any, metadata: any) =>
+	new InternalServerError({
+		metadata,
+		message: e.message,
+		originalError: e,
+	});
+
 export const httpRequest = async <T = any>(options: HttpRequest): Promise<Response<T>> => {
 	const { url, body, ...rest } = options;
 	rest.method = rest.method ?? HttpMethod.GET;
 
 	const response = await new Promise<Response>((resolve, reject) => {
 		const request = https.request(url, rest, async (res) => {
-			res.on('error', (e) =>
-				reject(
-					new InternalServerError({
-						message: e.message,
-						metadata: { options },
-						originalError: e,
-					}),
-				),
-			);
-
 			const chunks: any[] = [];
-			res.on('data', (chunk) => chunks.push(chunk));
 
+			res.on('error', (e) => reject(makeError(e, options)));
+			res.on('data', (chunk) => chunks.push(chunk));
 			res.on('end', () => {
 				const data = Buffer.concat(chunks).toString().trim();
 				const { statusCode = HttpStatusCode.OK, headers } = res;
@@ -55,15 +53,7 @@ export const httpRequest = async <T = any>(options: HttpRequest): Promise<Respon
 			});
 		});
 
-		request.on('error', (e) =>
-			reject(
-				new InternalServerError({
-					message: e.message,
-					metadata: { options },
-					originalError: e,
-				}),
-			),
-		);
+		request.on('error', (e) => reject(makeError(e, options)));
 
 		if (body?.trim()?.length) {
 			request.write(body);
