f2325f1ddb0605e9eec130975ce79e230f41ae2e
Vagner Cardoso <vagnercardosoweb@gmail.com>
2024-01-07 20:26:45 -0300

    fix: creation of migration with final prefix "down" and "up"
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- scripts/create-migration.sh
+++ scripts/create-migration.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
   echo "Please provide a name for the migration"
   echo "Example: ./scripts/create-migration.sh create_table_users"
   exit 1
@@ -11,8 +10,8 @@ fi
 
 prefix=$(date +%Y%m%d%H%M%S)
 
-touch "migrations/${prefix}_$1_up.sql"
+touch "migrations/${prefix}_$1.up.sql"
 echo "Created migrations/${prefix}_$1.up.sql"
 
-touch "migrations/${prefix}_$1_down.sql"
+touch "migrations/${prefix}_$1.down.sql"
 echo "Created migrations/${prefix}_$1.down.sql"
